b7fe6d5145fc547bad33291c84e44bf33ee967e8
Fahami Jemal <fahamijemal1@gmail.com>
2026-02-09 14:45:39 +0300

    Refactor advanced results middleware and decouple controllers

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,9 @@ const {
     getBootcampsInRadius,
     bootcampPhotoUpload
 } = require('../controllers/bootcamps');
+
+const advancedResults = require('../middleware/advancedResults');
+const Bootcamp = require('../models/Bootcamp');
 const router = express.Router();
 
 router.use('/:bootcampId/courses', courseRouter);
@@ -18,7 +21,7 @@ router.route('/radius/:zipcode/:distance').get(getBootcampsInRadius);
 router.route('/:id/photo').put(bootcampPhotoUpload);
 router.
 route('/')
-.get(getBootcamps)
+.get(advancedResults(Bootcamp,'courses'), getBootcamps)
 .post(createBootcamp);
 
 router.
